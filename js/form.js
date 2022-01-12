7f14cc7a13107a6897ebdc6181d9f1ec5dad54eb
Larissa <larissasousa.wrks@gmail.com>
2022-01-12 15:11:31 -0400

    nu functions

--- js/form.js
+++ js/form.js
@@ -1,38 +1,88 @@
 var btnAdd = document.querySelector('#adicionar-paciente')
+
+var tdImc = document.createElement('td')
+
 btnAdd.addEventListener('click', function (event) {
   event.preventDefault()
-  
 
-  var form = document.querySelector('#form-adiciona');
-   
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
+  var form = document.querySelector('#form-adiciona')
+  var patient = getPatientData(form)
+  var patientTr = createTr(patient)
 
-  var pacienteTr = document.createElement("tr")
+  var fatalError = patientValidate(patient)
 
-  var nomeTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td")
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
+  if (fatalError.length > 0) {
+    var errorMsg = document.querySelector("#error-message");
+    errorMsg.textContent = fatalError;
+    return;
+  }
 
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-  imcTd.textContent = imcCalc(peso, altura);
+  <li></li>
 
-  pacienteTr.appendChild(nomeTd)
-  pacienteTr.appendChild(pesoTd)
-  pacienteTr.appendChild(alturaTd)
-  pacienteTr.appendChild(gorduraTd)
-  pacienteTr.appendChild(imcTd)
+  var table = document.querySelector('#tabela-pacientes')
 
+  table.appendChild(patientTr)
 
-  var tabela = document.querySelector("#tabela-pacientes")
-
-  tabela.appendChild(pacienteTr)
-  
+  form.reset()
 })
+
+function showErrorMessage(fatalError) {
+  var ul = document.querySelector('#error-message')
+  fatalError.forEach(function (fatalError){
+    var li = document.createElement('li')
+    li.textContent = fatalError;
+    ul.appendChild(li)
+  })
+}
+
+function createTd(dado, classe) {
+  var td = document.createElement('td')
+  td.textContent = dado
+  td.classList.add(classe)
+
+  return td
+}
+
+function patientValidate(patient) {
+  var fatalError = [];
+
+  if (!weightValidate(patient.peso)) {
+    return ""
+  } else {
+    fatalError.push("Peso inválido")
+  }
+
+  if (!heightValidate(patient.altura)) {
+    return ""
+  } else {
+    fatalError.push("Altura inválida")
+  }
+
+  return fatalError;
+}
+
+function createTr(patient) {
+  var patientTr = document.createElement('tr')
+  patientTr.classList.add('patient')
+
+  patientTr.appendChild(createTd(patient.nome, 'info-nome'))
+  patientTr.appendChild(createTd(patient.peso, 'info-peso'))
+  patientTr.appendChild(createTd(patient.altura, 'info-altura'))
+  patientTr.appendChild(createTd(patient.gordura, 'info-gorduraTd'))
+  patientTr.appendChild(createTd(patient.imc, 'info-imc'))
+
+  return patientTr
+}
+
+tdImc.textContent = imcCalc(peso, altura)
+
+function getPatientData(form) {
+  var patient = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: imcCalc(form.peso.value, form.altura.value)
+  }
+  return patient
+}
---
+++ js/imc-calc.js
@@ -0,0 +1,63 @@
+var title = document.querySelector('.title')
+title.textContent = 'Bananinha Nutrissimy'
+
+var patients = document.querySelectorAll('.patient')
+
+function imcCalc(peso, altura) {
+  var imc = 0
+
+  imc = peso / (altura * altura)
+
+  return imc.toFixed(2)
+}
+
+
+function weightValidate(peso){
+  if(peso >= 0 && peso <=1000){
+    return true 
+  }else{
+    return false
+  }
+}
+
+function heightValidate(altura){
+  if(altura >= 0 && altura <= 3.0){
+    return true
+  }else{
+    return false
+  }
+}
+
+for (var i = 0; i < patients.length; i++) {
+  var patient = patients[i]
+
+  var tdPeso = patient.querySelector('.info-peso')
+  var peso = tdPeso.textContent
+
+  var tdAltura = patient.querySelector('.info-altura')
+  var altura = tdAltura.textContent
+
+  var tdImc = patient.querySelector('.info-imc')
+
+  var pesoValido = weightValidate(peso);
+  var alturaValida = heightValidate(altura);
+
+  if (!weightValidate) {
+    console.log('Peso inválido!')
+    pesoValido = false
+    tdImc.textContent = 'Peso inválido'
+    patient.classList.add('paciente-invalido')
+  }
+
+  if (!heightValidate) {
+    console.log('Altura inválida!')
+    alturaValida = false
+    tdImc.textContent = 'Altura inválida'
+    patient.classList.add('paciente-invalido')
+  }
+
+  if (alturaValida && pesoValido) {
+    var imc = imcCalc(peso, altura)
+    tdImc.textContent = imc
+  }
+}
